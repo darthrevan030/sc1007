888f822a5d9994e37b7d43ff897b9223cc3daa9f
darth.revan.030 <smartsamarth03@gmail.com>
2025-02-11 12:48:59 +0800

    Added Lab 1 Model Answers and Lab 2 Templates

---
+++ lab_1/Answer/Model-Answer/Q1-model-answer-version-01.py
@@ -0,0 +1,86 @@
+class ListNode:
+    def __init__(self, item):
+        self.item = item
+        self.next = None
+
+def printList(head):
+    print("Current List:", end=" ")
+    cur = head
+    while cur is not None:
+        print(cur.item, end=" ")
+        cur = cur.next
+    print()
+
+def findNode(head, index):
+    if head is None or index < 0:
+        return None
+    cur = head
+    while index > 0:
+        cur = cur.next
+        if cur is None:
+            return None
+        index -= 1
+    return cur
+
+def insertNode(ptrHead, index, item):
+    newNode = ListNode(item)
+    if index == 0:
+        newNode.next = ptrHead[0]
+        ptrHead[0] = newNode
+        return True
+
+    pre = findNode(ptrHead[0], index - 1)
+    if pre is not None:
+        newNode.next = pre.next
+        pre.next = newNode
+        return True
+    return False
+
+def removeNode(ptrHead, index):
+    if ptrHead[0] is None:
+        return False
+
+    if index == 0:
+        cur = ptrHead[0]
+        ptrHead[0] = cur.next
+        del cur
+        return True
+
+    pre = findNode(ptrHead[0], index - 1)
+    if pre is not None and pre.next is not None:
+        cur = pre.next
+        pre.next = cur.next
+        del cur
+        return True
+    return False
+
+if __name__ == "__main__":
+    head = None
+    ptrHead = [head]
+    size = 0
+
+    print("Enter a list of numbers, terminated by any non-digit character:")
+    while True:
+        try:
+            item = int(input())
+            if insertNode(ptrHead, size, item):
+                size += 1
+        except ValueError:
+            break
+
+    printList(ptrHead[0])
+
+    while True:
+        try:
+            index = int(input("Enter the index of the node to be removed: "))
+            if removeNode(ptrHead, index):
+                size -= 1
+                print("After the removal operation:")
+                printList(ptrHead[0])
+            else:
+                print("The node cannot be removed.")
+                break
+        except ValueError:
+            break
+
+    printList(ptrHead[0])
---
+++ lab_1/Answer/Model-Answer/Q4-model-answer-version-01.py
@@ -0,0 +1,91 @@
+class ListNode:
+    def __init__(self, num):
+        self.num = num
+        self.next = None
+
+def printList(head):
+    cur = head
+    while cur is not None:
+        print(cur.num, end=" ")
+        cur = cur.next
+    print()
+
+def findNode(head, index):
+    if head is None or index < 0:
+        return None
+    cur = head
+    while index > 0:
+        cur = cur.next
+        if cur is None:
+            return None
+        index -= 1
+    return cur
+
+def insertNode(ptrHead, index, value):
+    newNode = ListNode(value)
+    if ptrHead is None:
+        return newNode
+    if index == 0:
+        newNode.next = ptrHead
+        return newNode
+    cur = ptrHead
+    prev = None
+    count = 0
+    while cur is not None and count < index:
+        prev = cur
+        cur = cur.next
+        count += 1
+    if prev is not None:
+        prev.next = newNode
+        newNode.next = cur
+    return ptrHead
+
+def deleteList(ptrHead):
+    cur = ptrHead
+    while cur is not None:
+        temp = cur.next
+        cur.next = None  
+        cur = temp
+    return None
+
+def duplicateReverse(head, ptrNewHead):
+    if head is None:
+        ptrNewHead[0] = None
+        return 0
+    
+    # Create new list in reverse order
+    cur = head
+    while cur is not None:
+        newNode = ListNode(cur.num)
+        newNode.next = ptrNewHead[0]
+        ptrNewHead[0] = newNode
+        cur = cur.next
+    return 0
+
+if __name__ == "__main__":
+    head = None
+    dupRevHead = [None]
+    
+    print("Enter a list of numbers, terminated by any non-digit character:")
+    print("Enter any non-digit character to finish input:")
+    try:
+        while True:
+            item = int(input())
+            head = insertNode(head, 0, item)  
+    except ValueError:
+        pass
+
+    print("\nBefore duplicateReverse() is called:")
+    print("The original list:", end=" ")
+    printList(head)
+    
+    duplicateReverse(head, dupRevHead)
+    
+    print("\nAfter duplicateReverse() was called:")
+    print("The original list:", end=" ")
+    printList(head)
+    print("The duplicated reverse list:", end=" ")
+    printList(dupRevHead[0])
+    
+    head = deleteList(head)
+    dupRevHead[0] = deleteList(dupRevHead[0])
