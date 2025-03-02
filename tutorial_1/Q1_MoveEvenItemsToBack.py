a20346e1869236668028c7f1c3ea306b57752834
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-02 15:02:38 +0800

    Removed Redundant Files from Tutorial 1

--- tutorial_1/Q1_MoveEvenItemsToBack.py
+++
@@ -1,9 +0,0 @@
-def moveEvenItemsToBack(ll):
-    if ll.size < 2:
-        return
-    
-    cur = ll.head
-    index = 0
-    for _ in range(ll.size):
-        if cur.item % 2 == 0:
-            even_value
