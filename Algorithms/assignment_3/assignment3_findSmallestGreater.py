3db096a78ef62ee3d8bb390b12d5c1b7e56f152b
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-29 17:09:58 +0800

    Added Visible Test Cases

--- Algorithms/assignment_3/assignment3_findSmallestGreater.py
+++ Algorithms/assignment_3/assignment3_findSmallestGreater.py
@@ -1,2 +1,12 @@
 def find_smallest_greater(arr, x):
     #insert your codes here
+if __name__ == "__main__":
+    #Visible Test Case 1
+    arr = [1, 2, 3, 4, 5]
+    result = find_smallest_greater(arr, 9)
+    print(result)
+    
+    # Visible Test Case 2
+    arr2 = [1, 2, 4, 5, 7, 9]
+    result2 = find_smallest_greater(arr2, 4)
+    print(result2)
