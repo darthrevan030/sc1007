2f3b314c8425f37595c12295bc30ac513e31f5d6
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-29 17:13:05 +0800

    Added docstring to clarify the Instructions and constraints of the find_smallest_greater function

--- Algorithms/assignment_3/assignment3_findSmallestGreater.py
+++ Algorithms/assignment_3/assignment3_findSmallestGreater.py
@@ -1,5 +1,17 @@
+'''
+You are given a sorted list of integers (no duplicates) and a target number. Your task is to find the 
+smallest number in the list that is greater than the target. 
+If such a number does not exist, return -1.
+
+Time Limit: 0.05s for each input
+Memory Limit: 256 MB
+Source Limit: 1024 KB
+'''
+
+
 def find_smallest_greater(arr, x):
     #insert your codes here
+
 if __name__ == "__main__":
     #Visible Test Case 1
     arr = [1, 2, 3, 4, 5]
