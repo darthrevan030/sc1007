1836e77113a03b41fff085836d15ddd83c5636d3
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-29 15:05:16 +0800

    Added Instructions and limits for the question

--- Algorithms/assignment_3/assignment3_firstOccurence.py
+++ Algorithms/assignment_3/assignment3_firstOccurence.py
@@ -1,3 +1,21 @@
+'''
+First occurrence in a sorted array:
+You are given a sorted array of integers (possibly with duplicates). Write a function to find the index
+of the first occurrence of a given target number. If the target is not present, return -1.
+Consider using binary search based algorithm as brute force algorithm will report "Time Limit exceeded" 
+for some test cases. 
+
+Time Limit: 0.05s for each input
+Memory Limit: 256 MB
+Source Limit: 1024 KB
+'''
+
 def first_occurrence(arr, target):
     #insert your codes
-    
+
+
+
+if __name__ == "__main__":
+    arr = [1, 2, 2, 2, 3, 4]
+    result = first_occurrence(arr, 2)
+    print(result)
