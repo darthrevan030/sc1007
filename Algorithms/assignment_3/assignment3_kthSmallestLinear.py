708e04b49e1838f0c55b67fc0dd38fe6ee26c09e
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-29 18:06:43 +0800

    Added docstring to describe the k-th smallest element function and its constraints

--- Algorithms/assignment_3/assignment3_kthSmallestLinear.py
+++ Algorithms/assignment_3/assignment3_kthSmallestLinear.py
@@ -1,2 +1,19 @@
+'''
+Given an n by n matrix (2D array) where each row and column is sorted in non-decreasing order, find the k-th smallest element. 
+
+For example, given the 3 by 3 matrix: 
+
+            1  5  9
+            10 11 13
+            12 13 15
+
+The 5th smallest number is 11. The 7th and 8th smallest numbers are both 13.  
+
+Time Limit: 0.05s for each input
+Memory Limit: 256 MB
+Source Limit: 1024 KB
+'''
+
+
 def kth_smallest(matrix, k):
     #insert your codes here
